3b215dd2ec2dd1db35b14022adbe125b8813c025
canyzhou <canyzhou@tencent.com>
2026-04-18 15:27:14 +0800

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development';
+
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
-  assetPrefix: '/eygpt-travel-guide',
+  assetPrefix: isDev ? '' : '/eygpt-travel-guide',
   images: {
     unoptimized: true
   }
